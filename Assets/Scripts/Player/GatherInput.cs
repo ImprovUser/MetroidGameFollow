3afacf729690f9162698b643a7464509bfe66d7c
Max <60143006+ImprovUser@users.noreply.github.com>
2025-11-10 22:41:48 -0500

    added MovementRead

---
+++ Assets/Scripts/Player/GatherInput.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.InputSystem; 
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public PlayerInput playerInput;
+
+    private InputActionMap playerMap;
+    private InputActionMap uiMap;
+
+    public InputActionReference jumpActionRef;
+    public InputActionReference moveActionRef;
+
+    private float horizontalInput;
+
+    private void OnEnable()
+    {
+        jumpActionRef.action.performed += TryToJump;
+        jumpActionRef.action.canceled += StopJump;
+    }
+
+    private void OnDisable()
+    {
+        jumpActionRef.action.performed -= TryToJump;
+        jumpActionRef.action.canceled -= StopJump;
+        playerMap.Disable();
+    }
+
+    private void TryToJump(InputAction.CallbackContext value)
+    {
+        Debug.Log("Jump action triggered");
+    }
+
+    private void StopJump(InputAction.CallbackContext value)
+    {
+        Debug.Log("Jump action stopped");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        playerMap = playerInput.actions.FindActionMap("Player");
+        uiMap = playerInput.actions.FindActionMap("UI");
+        playerMap.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = moveActionRef.action.ReadValue<float>();
+        Debug.Log("Horizontal Input: " + horizontalInput);
+    }
+}
