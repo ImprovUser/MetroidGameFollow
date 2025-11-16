66ae2ba77c453a2cf61bf857b4149b723cbc1cce
Max <60143006+ImprovUser@users.noreply.github.com>
2025-11-16 13:29:43 -0500

    Added Ladder, Dash + Animations

--- Assets/Scripts/Ladders.cs
+++ Assets/Scripts/Ladders.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class Ladders : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private LadderAbility laddersAbility;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        laddersAbility=collision.GetComponent<LadderAbility>();
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(laddersAbility!=null)
+        {
+            if (laddersAbility.isPermitted)
+                laddersAbility.canGoOnLadder = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if(laddersAbility!= null)
+        {
+            if(laddersAbility.isPermitted)
+                laddersAbility.canGoOnLadder = false;
+        }
     }
 }
--- Assets/Scripts/Player/LadderAbility.cs
+++ Assets/Scripts/Player/LadderAbility.cs
@@ -10,11 +10,16 @@ public class LadderAbility : BaseAbility
     private bool climb;
     public bool canGoOnLadder;
 
+    private string ladderParameterName = "Ladder";
+    private int ladderParameterID;
+    
+
 
 
     protected override void Initialization()
     {
         base.Initialization();
+        ladderParameterID = Animator.StringToHash(ladderParameterName);
         minimumLadderTime = setMinLadderTime;
     }
 
@@ -34,6 +39,7 @@ public class LadderAbility : BaseAbility
     {
         if (!isPermitted)
             return;
+        linkedAnimator.enabled = true;
         if (linkedStateMachine.currentState == PlayerStates.State.Ladders || linkedStateMachine.currentState == PlayerStates.State.Dash
             || !canGoOnLadder)
             return;
@@ -43,6 +49,7 @@ public class LadderAbility : BaseAbility
         linkedPhysics.ResetVelocity();
         climb = true;
         minimumLadderTime = setMinLadderTime;
+        
     }
     private void StopClimb(InputAction.CallbackContext value)
     {
@@ -52,11 +59,52 @@ public class LadderAbility : BaseAbility
             return;
 
         linkedPhysics.ResetVelocity();
+        linkedAnimator.enabled = false;
     }
 
+    public override void ExitAbility()
+    {
+        linkedPhysics.EnableGravity();
+        climb = false;
+        linkedAnimator.enabled = true;
+    }
+    public override void ProcessAbility()
+    {
+        if (climb)
+            minimumLadderTime -= Time.deltaTime;
+
+        if(linkedInput.horizontalInput!=0)
+        {
+            linkedStateMachine.ChangeState(PlayerStates.State.Jump);
+            //linkedPhysics.EnableGravity();
+            return;
+        }
+
+        if(canGoOnLadder == false)
+        {
+            if(linkedPhysics.grounded == false)
+            {
+                linkedStateMachine.ChangeState(PlayerStates.State.Jump);
+               // linkedPhysics.EnableGravity();
+            }
+        }
+
+        if(linkedPhysics.grounded && minimumLadderTime<=0)
+        {
+            linkedStateMachine.ChangeState(PlayerStates.State.Idle);
+           // linkedPhysics.EnableGravity();
+
+        }
+
+    }
     public override void ProcessFixedAbility()
     {
         if (climb)
             linkedPhysics.rb.linearVelocity = new Vector2(0, linkedInput.verticalInput * climbSpeed);
+    }
+
+    public override void UpdateAnimator()
+    {
+        linkedAnimator.SetBool(ladderParameterID, linkedStateMachine.currentState == PlayerStates.State.Ladders);
     }
 }
